0737b6a490d0495957bd85009f8a431090540c1d
Krishnan Shankar <krishnans2006@gmail.com>
2024-01-23 06:58:43 -0500

    Implement parsing raw score point values

--- scripts/gradebook.js
+++ scripts/gradebook.js
@@ -1,3 +1,12 @@
+const headerHierarchy = [
+    ":scope",
+    ".dx-datagrid-headers",
+    ".dx-datagrid-content",
+    ".dx-datagrid-table",
+    "tbody",
+]
+
+
 const datagridHierarchy = [
     ":scope",
     ".dx-datagrid-rowsview",
@@ -15,11 +24,42 @@ const script = () => {
 
     if (assignments) {
         const datagrid = assignments.querySelector(":scope > .dx-datagrid");
+        const headers = datagrid.querySelector(headerHierarchy.join(" > "));
         const rows = datagrid.querySelectorAll(datagridHierarchy.join(" > "));
 
+        let myPoints = 0;
+        let totalPoints = 0;
+
         rows.forEach(row => {
-            console.log(row.children[0].innerHTML);
+            const cells = row.querySelectorAll("td");
+            const fields = {
+                "Date": null,
+                "Score Type": null,
+                "Points": null,
+            };
+            cells.forEach(cell => {
+                const describedBy = cell.getAttribute("aria-describedby");
+                if (!describedBy) {
+                    return;
+                }
+                const field = headers.querySelector(`#${describedBy} > .dx-datagrid-text-content`).innerText;
+                if (field in fields) {
+                    fields[field] = cell.innerText;
+                }
+            });
+
+            if (fields["Score Type"] === "Raw Score") {
+                const points = fields["Points"];
+                if (points.includes("Possible")) {
+                    return;  // skip this row
+                }
+                const splitPoints = points.split("/");
+                myPoints += parseFloat(splitPoints[0]);
+                totalPoints += parseFloat(splitPoints[1]);
+            }
         })
+
+        console.log(`My points: ${myPoints}, Total points: ${totalPoints}, Grade: ${myPoints / totalPoints * 100}%`);
     } else {
         console.log("No assignments found.");
     }
