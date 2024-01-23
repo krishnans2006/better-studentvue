b194004d5f67255191eb023a0015fce3c240996c
Krishnan Shankar <krishnans2006@gmail.com>
2024-01-23 06:09:22 -0500

    Add attempt at basic gradebook parsing

--- scripts/gradebook.js
+++ scripts/gradebook.js
@@ -1,1 +1,24 @@
-console.log("gradebook.js")
+const datagridHierarchy = [
+    ":scope",
+    ".dx-datagrid-rowsview",
+    ".dx-scrollable-wrapper",
+    ".dx-scrollable-container",
+    ".dx-scrollable-content",
+    ".dx-datagrid-content",
+    ".dx-datagrid-table",
+    "tbody",
+    "tr",
+]
+
+const assignments = document.querySelector("#AssignmentsGrid")
+
+if (assignments) {
+    const datagrid = assignments.querySelector(":scope > .dx-datagrid");
+    const rows = datagrid.querySelectorAll(datagridHierarchy.join(" > "));
+
+    rows.forEach(row => {
+        console.log(row.children[0].innerHTML);
+    })
+} else {
+    console.log("No assignments found.");
+}
